ff22e6ce719dc99707d29299aebf057b0593c68e
recpasta <send-crafter-pasta@duck.com>
2024-10-05 23:26:40 -0700

    Update keymap.c

--- keyboards/massdrop/carina/keymaps/rgb_matrix_caps/keymap.c
+++ keyboards/massdrop/carina/keymaps/rgb_matrix_caps/keymap.c
@@ -19,18 +19,25 @@ keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, \
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_GRV, KC_MENU, KC_RCTRL, MO(1) \
+        KC_CAPS, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, MO(1), KC_MENU, KC_RCTRL \
     ),
     [1] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,   _______, _______, U_T_AGCR,\
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, KC_INS,  KC_HOME, KC_PGUP, _______,   _______,         KC_AUDIO_VOL_UP, \
-        _______, RGB_TOG,  _______, _______, _______, MD_BOOT, TG_NKRO, KC_DEL,  KC_END,  KC_PGDN,   KC_UP,                   KC_AUDIO_VOL_DOWN, \
-        OW, _______, _______,                   DBG_FAC,                                    KC_LEFT, KC_DOWN, KC_RIGHT,  _______ \
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_UP,   _______, MO(2), _______,\
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______,  KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,         _______, \
+        _______, RGB_TOG,  _______, _______, _______, _______, _______, _______,  KC_END,  KC_PGUP, KC_PGDN,                  _______, \
+        KC_VOLD, KC_VOLU, KC_MUTE,                   _______,                                    KC_PGUP, _______, KC_PGND, _______ \
+    ),
+    [2] = LAYOUT(
+        _______  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+        _______, _______, _______, _______, _______, MD_BOOT, _______, _______, _______, _______, _______,                   _______, \
+        OW, _______, U_T_AGCR,                   DBG_FAC,                            _______, _______, _______,          _______  \
     ),
     /*
     [X] = LAYOUT(
